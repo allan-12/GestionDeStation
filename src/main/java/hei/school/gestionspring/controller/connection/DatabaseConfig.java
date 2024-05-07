e38d48661e7a0fddede653d6ad0a72ea5dcebb82
= <=>
2024-05-07 21:40:28 +0300

    feat: added an conneciton for the project

---
+++ src/main/java/hei/school/gestionspring/controller/connection/DatabaseConfig.java
@@ -0,0 +1,19 @@
+package hei.school.gestionspring.controller.connection;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+@Component
+public class DatabaseConfig {
+    @Bean
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(
+                Config.URL,
+                Config.USERNAME,
+                Config.PASSWORD
+        );
+    }
+}
